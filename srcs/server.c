1133eda502a75e838a4dba386e5ed09dc20a4155
Imrane-Halim <ihalim.professionel@gmail.com>
2024-09-29 13:20:05 +0000

    added server side

--- srcs/server.c
+++ srcs/server.c
@@ -1,8 +1,32 @@
 #include "../inc.h"
 
-int	main()
+void    to_string(int sig)
 {
-	ft_printf("the server PID: %d", getpid());
-    //ft_printf("199");
+    static short    current_bit;
+    static char     c;
+
+    if (sig == SIGUSR1)
+        c |= (1 << current_bit);
+    else if (sig == SIGUSR2)
+        c &= ~(1 << current_bit);
+
+    current_bit++;
+    if (current_bit == 8)
+    {
+        ft_printf("%c", c);
+        c = 0;
+        current_bit = 0;
+    }
 }
 
+int	main()
+{
+    ft_printf("server PID: %d\n\n", getpid());
+
+    signal(SIGUSR1, to_string);
+    signal(SIGUSR2, to_string);
+
+    while (1)
+        pause();
+    return (0);
+}
